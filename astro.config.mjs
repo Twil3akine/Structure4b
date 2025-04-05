319313a39c44782c8e89a9df3642604ec0d813f7
Twil3akine <twil3akine@gmail.com>
2025-04-05 23:29:47 +0900

    fix(hot-reload): fix hot reload issue

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,26 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    integrations: [
+        mdx(),
+    ],
+    markdown: {
+        shikiConfig: {
+            theme: 'github-dark',
+            // @ts-ignore
+            langs: ['c'],
+        },
+    },
+    vite: {
+        server: {
+            host: '0.0.0.0',
+            watch: {
+                usePolling: true,
+                interval: 2000,
+            },
+        },
+    }
+});
