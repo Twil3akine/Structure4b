d5bf0fca83af8a5d63bb3de05cf3c7907e0f6182
Twil3akine <twil3akine@gmail.com>
2025-04-06 09:55:38 +0900

    feat(layout): apply Layout.astro to all pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+// @ts-ignore
+import { fileURLToPath } from 'url';
 
 // https://astro.build/config
 export default defineConfig({
@@ -19,8 +21,13 @@ export default defineConfig({
             host: '0.0.0.0',
             watch: {
                 usePolling: true,
-                interval: 2000,
+                interval: 500,
             },
         },
+        resolve: {
+            alias: {
+                '@': fileURLToPath(new URL('./src', import.meta.url)),
+            }
+        }
     }
 });
